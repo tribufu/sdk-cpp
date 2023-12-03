11888e15729ef6a3f656f31787a058d054be5988
Guilherme Werner <guilherme.werner@tribufu.com>
2023-12-03 13:18:17 -0300

    Update msbuild.ps1

--- scripts/msbuild.ps1
+++ scripts/msbuild.ps1
@@ -2,3 +2,9 @@
 
 # Windows (x86_64)
 msbuild /p:Configuration="debug" /p:Platform="windows-x86_64"
+
+# Windows (i686)
+msbuild /p:Configuration="debug" /p:Platform="windows-i686"
+
+# Windows (aarch64)
+msbuild /p:Configuration="debug" /p:Platform="windows-aarch64"
