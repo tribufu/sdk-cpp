c092692ed60e4a170a89167af980962ca10560f4
Guilherme Werner <guilherme.werner@tribufu.com>
2023-12-16 09:35:29 -0300

    Use premake from mintaka submodule

--- scripts/premake.sh
+++ scripts/premake.sh
@@ -2,9 +2,9 @@
 
 if [ "$(expr substr $(uname -s) 1 5)" = "Linux" ]
 then
-    ./vendor/premake-core/linux/premake5 gmake2
+    ./vendor/Mintaka/vendor/premake-core/linux/premake5 gmake2
 
 elif [ "$(uname)" = "Darwin" ]
 then
-    ./vendor/premake-core/mac/premake5 xcode4
+    ./vendor/Mintaka/vendor/premake-core/mac/premake5 xcode4
 fi
